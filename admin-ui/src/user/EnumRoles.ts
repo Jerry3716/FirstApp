0726633679f9c8940fd37cdc8dc91cf6f5813e7f
Jerry3716 <35403539+Jerry3716@users.noreply.github.com>
2022-02-10 22:27:54 -0500

    amplication-build-ckzhun97385825701s6g7dlytxb

---
+++ admin-ui/src/user/EnumRoles.ts
@@ -0,0 +1,6 @@
+export enum EnumRoles {
+  User = "user",
+  Reader = "reader",
+  PowerUser = "powerUser",
+  Admin = "admin",
+}
---
+++ admin-ui/src/user/roles.ts
@@ -0,0 +1,18 @@
+export const ROLES = [
+  {
+    name: "user",
+    displayName: "User",
+  },
+  {
+    name: "reader",
+    displayName: "Reader",
+  },
+  {
+    name: "powerUser",
+    displayName: "Power User",
+  },
+  {
+    name: "admin",
+    displayName: "Admin",
+  },
+];
